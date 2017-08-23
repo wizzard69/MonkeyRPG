0022680751d5c1c39f180fe93f98aea395b7e24f
WizBit <dean@monkiezone.com>
2017-08-23 16:29:49 +0100

    Commit End of EP1

---
+++ 1.0/MonkeyRPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+    PlayerMotor motor;
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit,100, movementMask))
+            {
+                motor.Move2Point(hit.point);
+            }
+        }
+
+		if (Input.GetMouseButtonDown(1))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+			 
+			}
+		}
+    }
+}
